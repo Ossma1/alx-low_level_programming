79b7974363727222d2c1fccfa49831ed173a7e9e
oussama <toumi10oussama@gmail.com>
2023-03-22 13:18:29 -0700

    for

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**print alphabet
+ * suivant d'un line 
+ ***/
+
+ void print_alphabet(void){
+ char lettre;
+ for(lettre ='a'; lettre <= 'z' ; lettre++)
+	 _putchar(lettre);
+	 
+ _putchar('\n');
+}
+ 
