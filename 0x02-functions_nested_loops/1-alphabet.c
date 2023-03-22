f950d0e23060cb747a95497d76cb0df750aafc70
oussama <toumi10oussama@gmail.com>
2023-03-22 16:31:31 -0700

    loop des caractere

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,15 @@
 #include "main.h"
-/**print alphabet
+/**
  * suivant d'un line 
- ***/
+ */
 
- void print_alphabet(void){
- char lettre;
- for(lettre ='a'; lettre <= 'z' ; lettre++)
-	 _putchar(lettre);
+void print_alphabet(void)
+{
+	char lettre;
+
+	for (lettre = 'a'; lettre <= 'z' ; lettre++)
+		_putchar(lettre);
 	 
- _putchar('\n');
+	_putchar('\n');
 }
  
