a999e8c250a406bd6a82ce6da72772550133db8e
oussama <toumi10oussama@gmail.com>
2023-03-22 18:09:39 -0700

    ajouter position de for

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,6 +9,7 @@
 void print_to_98(int n)
 {
 	int i, j;
+	
 	if (n <= 98)
 	{
 		for (i = n; i <= 98; i++)
@@ -20,7 +21,7 @@ void print_to_98(int n)
 		}
 	} else if (n >= 98)
 	{
-	for (j = n; j >= 98; j--)
+		for (j = n; j >= 98; j--)
 		{
 			if (j != 98)
 				printf("%d, ", j);
