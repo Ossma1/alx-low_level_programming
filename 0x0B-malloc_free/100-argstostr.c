a360fc1d039862963b905bdfd1ef0511d9b5bab3
oussama <toumi10oussama@gmail.com>
2023-04-11 19:07:53 -0700

    adenced tasks

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -2,44 +2,50 @@
 #include "main.h"
 
 /**
-* *argstostr - concatenates all the arguments of the program
-* @ac: number of arguments
-* @av: array of arguments
-*
-* Return: Pointer to the new string (Success), NULL (Error)
-*/
-char *argstostr(int ac, char **av)
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
+ *
+ * Return: pointer to the newly allocated memory block
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int i, j, k, len;
-	char *str;
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+	if (new_size == old_size)
+		return (ptr);
 
-	for (i = 0; i < ac; i++)
+	if (new_size == 0 && ptr)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
+		free(ptr);
+		return (NULL);
 	}
 
-	str = malloc(sizeof(char) * (len + 1));
+	if (!ptr)
+		return (malloc(new_size));
 
-	if (str == NULL)
+	ptr1 = malloc(new_size);
+	if (!ptr1)
 		return (NULL);
 
-	k = 0;
+	old_ptr = ptr;
 
-	for (i = 0; i < ac; i++)
+	if (new_size < old_size)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k] = '\n';
-		k++;
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
 	}
-	
-	return (str);
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
+}
 }
